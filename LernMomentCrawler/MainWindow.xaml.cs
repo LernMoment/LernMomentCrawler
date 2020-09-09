646d2afdf4374223be7be556cf224848a302cd9f
Jan Suchotzki <jan@lernmoment.de>
2020-09-09 22:00:10 +0200

    cleanup

--- LernMomentCrawler/MainWindow.xaml.cs
+++ LernMomentCrawler/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace LernMomentCrawler
@@ -24,29 +11,30 @@ namespace LernMomentCrawler
     public partial class MainWindow : Window
     {
         private DispatcherTimer _timer;
-        private TimeSpan _time;
+        private TimeSpan _secondsSinceStart;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            _timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, delegate
+            var handler = new EventHandler((sender, args) =>
             {
-                timerView.Text = _time.ToString("c");
-                _time = _time.Add(TimeSpan.FromSeconds(1));
-            }, Application.Current.Dispatcher);
+                timerView.Text = _secondsSinceStart.ToString("c");
+                _secondsSinceStart = _secondsSinceStart.Add(TimeSpan.FromSeconds(1));
+            });
+
+            _timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, handler, Application.Current.Dispatcher);
         }
 
         private void LoadWebSiteButton_Click(object sender, RoutedEventArgs e)
         {
-            using var wClient = new WebClient();
-            var result = wClient.DownloadString("http://localhost:63093/lernmoment/10");
+            using var client = new WebClient();
+            var result = client.DownloadString("http://localhost:63093/lernmoment/10");
             resultHtmlView.Text = result;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            _time = new TimeSpan(0);
+            _secondsSinceStart = new TimeSpan(0);
             _timer.Start();
         }
     }
